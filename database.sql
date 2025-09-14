cd4c594a8ee7e457025d46aac0753763dcf48ada
Li Zhao-Zhi <lizhaozhi2017@163.com>
2025-09-14 16:53:27 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,6 @@
 -- Author: Li Zhao-Zhi
+-- Create the database based on schema
+
 -- create Fraud_Detection_eCommerce database
 DROP DATABASE IF EXISTS "Fraud_Detection_eCommerce";
 CREATE DATABASE "Fraud_Detection_eCommerce"
@@ -17,16 +19,16 @@ COMMENT ON DATABASE "Fraud_Detection_eCommerce"
 
 -- create CUSTOMERS table
 CREATE TABLE IF NOT EXISTS customers(
-	customer_id UUID PRIMARY KEY NOT NULL,
-	age INT CHECK (age >= 0),
+	customer_id TEXT PRIMARY KEY NOT NULL,
+	age TEXT --CHECK (age >= 0),
 	customer_location VARCHAR(100),
-	account_age_days INT CHECK (account_age_days >=0)
+	account_age_days INT --CHECK (account_age_days >=0)
 );
 
 -- create TRANSACTIONS table
 CREATE TABLE IF NOT EXISTS transactions(
-    transaction_id UUID PRIMARY KEY,
-    customer_id UUID REFERENCES customers(customer_id),
+    transaction_id TEXT PRIMARY KEY,
+    customer_id TEXT REFERENCES customers(customer_id),
     transaction_amount NUMERIC(10,2),
     transaction_date TIMESTAMP,
     payment_method VARCHAR(50),
