b0f94c8de3cc05d14f5cd540d8bd70a24e2c0494
Li Zhao-Zhi <lizhaozhi2017@163.com>
2025-09-14 14:27:55 +0800

    Upload files

---
+++ database.sql
@@ -0,0 +1,41 @@
+-- Author: Li Zhao-Zhi
+-- create Fraud_Detection_eCommerce database
+DROP DATABASE IF EXISTS "Fraud_Detection_eCommerce";
+CREATE DATABASE "Fraud_Detection_eCommerce"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United Kingdom.936'
+    LC_CTYPE = 'English_United Kingdom.936'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+COMMENT ON DATABASE "Fraud_Detection_eCommerce"
+    IS 'Fraud Detection in e-Commerce Dataset: https://www.kaggle.com/datasets/kevinvagan/fraud-detection-dataset';
+
+-- create CUSTOMERS table
+CREATE TABLE IF NOT EXISTS customers(
+	customer_id UUID PRIMARY KEY NOT NULL,
+	age INT CHECK (age >= 0),
+	customer_location VARCHAR(100),
+	account_age_days INT CHECK (account_age_days >=0)
+);
+
+-- create TRANSACTIONS table
+CREATE TABLE IF NOT EXISTS transactions(
+    transaction_id UUID PRIMARY KEY,
+    customer_id UUID REFERENCES customers(customer_id),
+    transaction_amount NUMERIC(10,2),
+    transaction_date TIMESTAMP,
+    payment_method VARCHAR(50),
+    product_category VARCHAR(50),
+    quantity INT,
+    device_used VARCHAR(50),
+    ip_address VARCHAR(50),
+    shipping_address TEXT,
+    billing_address TEXT,
+    is_fraudulent BOOLEAN,
+    transaction_hour INT
+);
